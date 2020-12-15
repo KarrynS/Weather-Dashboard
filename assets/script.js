ddf335db62eba827c368659da1b2e17f00c0ea57
Phoenix <73591532+KarrynS@users.noreply.github.com>
2020-12-16 02:49:29 +1100

    Ajax calls and appending retrieved info to DOM

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   var APIKey = "2a058d9c1c6b248fc7317f20f31266dc";
   var savedCities = [];
-  var currentDay = moment().format('dddd, D MMMM YYYY')
+  var today = moment().format('dddd, D MMMM YYYY')
  
 //Rendering Search Function and adding previous search to list
   function renderSearch () {
@@ -10,8 +10,8 @@ $(document).ready(function() {
   $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     var citySearch = $("#searchFormInput").val().trim();
-    console.log(citySearch);
     savedCities.push(citySearch);
+    console.log(citySearch);
     console.log(savedCities);
 
     $("#savedCityDiv").empty();
@@ -24,71 +24,70 @@ $(document).ready(function() {
     })
   }
   renderSearch();
+  
 
-//Handling Event for loading new city search
-function citySearch (event) {
+  //Handling request for new city search
+  function newCitySearch () {
+    var cityInput = "Sydney"
+    console.log(location)
+    //Empyting main content div
+    $("#citySearchDiv").empty();
+    $("#cityForecastDiv").empty();
+
+    
+    //Running AJAX call to retrieve OpenWeatherMap API parameters for searched city
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" +cityInput+ "&units=metric&appid=" +APIKey,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        //Creating variables and retriving required info from response
+        var currentCity = response.name;
+        
+        var currentTemp = $("<div>Temperature: " + response.main.temp + "ºC" + "</div>");
+        var currentHumidity = $("<div>Humidity: " + response.main.humidity + "%" + "</div>");
+        var currentWind = $("<div>Wind: " + response.wind.speed + "m/s" + "</div>");
+        var currentLat = response.coord.lat;
+        var currentLon = response.coord.lon;
+        var currentCityHeader = $("<div>");
+
+        //Displaying weather data to HTML
+        currentCityHeader.append($("<h1>" + currentCity + "</h1>"));
+        currentCityHeader.append($("<span>" + today + "</span>"));
+        $("#citySearchDiv").append(currentTemp);
+        $("#citySearchDiv").append(currentHumidity);
+        $("#citySearchDiv").append(currentWind);
+        $("#citySearchDiv").prepend(currentCityHeader);
+       
+    //Running AJAX call to retrieve UV index
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/uvi?lat=" +currentLat+ "&lon=" + currentLon + "&appid=" +APIKey,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      var currentIndex = $("<div id='index'>UV Index: " + response.value + "</div>");
+      $("#citySearchDiv").append(currentIndex);
+    });
+  })
+ 
+  }
+  newCitySearch();
+  
+  //Saving to local storage
+  localStorage.setItem("currentCity", cityInput);
 
 
-}
-citySearch();
+
+
+
+
 
 });
 
 
 
 
-
-/*
-//Function to update saved locations
-var searchUpdate = function(currentCity) {
-  if (currentCity !== "") {
-          savedCities.push(currentCity);
-          console.log(savedCities);
-
-  if (savedCities.length > 10) {
-          savedCities.shift();
-  }
-  
-  //Registering seraching for new city on submit, click or reload
-  $("#savedCitiesDiv").empty();
-  savedCities.forEach(function(city) {
-      $("#savedCitiesDiv").prepend($(`<div class="savedCity">${city}</div>`))
-    })
-  }
-}
-//searchUpdate(currentCity);
-   
-// Handle the event for loading data for a new city
-var startNewSearch = function(event) {
-  // If this was triggered by an event rather than page load,
-  if( event ) {
-    event.preventDefault();
-    // Grab input for current city search depending on whether it was a submit event or click event
-    if( event.type === "submit" ){
-      var citySearch = event.target.children[0].value;
-    } else if ( event.type === "click" ){
-      var citySearch = event.target.innerHTML;
-    }
-    
-  // Else if this is triggered by page load,
-  } else {
-    // Load last searched city if it exists, otherwise load Tokyo
-    if( localStorage.getItem("currentCity") ){
-      var citySearch = localStorage.getItem("currentCity");
-    } else {
-      var citySearch = "Melbourne";
-    }
-  }}
-
-  //Running AJAX call to retrieve OpenWeatherMap API parameters for searched city
-  //$.ajax({
-  //    url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" +APIKey,
-  //    method: "GET"
-  //}).then(function(response) {
-  //    console.log(response);
-  //})
-
-*/
 
 /*
     
