a642348f6bdcba778e5f2434506cfed06c0d993f
Phoenix <73591532+KarrynS@users.noreply.github.com>
2020-12-18 21:02:54 +1100

    cleaning up structure of script

--- assets/script.js
+++ assets/script.js
@@ -5,24 +5,22 @@ $(document).ready(function() {
   var today = moment().format('dddd, D MMMM YYYY')
   var cityInput; 
  
-//Rendering Search Function and adding previous search to list
+  //Rendering Search Function and adding previous search to list
   function renderSearch () {
     $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     var citySearch = $("#searchFormInput").val().trim();
 
     //Retrieving from local storage and updating search result
-    savedCities = localStorage.getItem("savedCitiesArr");
+    savedCities = localStorage.getItem("savedCities");
     savedCities = JSON.parse(savedCities);
     if ( !savedCities ){
       savedCities = [];
     }
     savedCities.push(citySearch);
-    console.log(citySearch);
-    console.log(savedCities);
-    
+
     //Saving previous searches to local storage
-    localStorage.setItem("savedCitiesArr", JSON.stringify(savedCities));
+    localStorage.setItem("savedCities", JSON.stringify(savedCities));
 
     $("#savedCityDiv").empty();
     //storedCities = localStorage.getItem(savedCities);
@@ -36,13 +34,14 @@ $(document).ready(function() {
   }
   renderSearch();
 
-  //Adding Event Listeners for new city search
+  //Adding Event Listeners for new city search on form 
   $("#searchBtn").on("click",function () {
     cityInput = $("#searchFormInput").val().trim();
     console.log("search button" + cityInput)
     newCitySearch();
   });
   
+  //Adding Event Listeners on previous searches
   $(document).on("click", ".savedCity", function () {
     cityInput = $(this).attr("data-name");
     console.log("saved list" +cityInput)
@@ -61,23 +60,20 @@ $(document).ready(function() {
         url: "https://api.openweathermap.org/data/2.5/weather?q=" +cityInput+ "&units=metric&appid=" +APIKey,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
-        console.log(response.weather[0].icon);
-
+       
         //Creating variables and retrieving required info from response
         var currentCity = response.name;
-        var currentTemp = $("<div>Temperature: " + response.main.temp + "ºC" + "</div>");
-        var currentHumidity = $("<div>Humidity: " + response.main.humidity + "%" + "</div>");
-        var currentWind = $("<div>Wind Speed: " + response.wind.speed + "m/s" + "</div>");
-        var currentIcon = $("<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'/>")
+        var currentTemp = $("<div class='temp'>Temperature: " + response.main.temp + "ºC" + "</div>");
+        var currentHumidity = $("<div class='humidity'>Humidity: " + response.main.humidity + "%" + "</div>");
+        var currentWind = $("<div class='windspeed'>Wind Speed: " + response.wind.speed + "m/s" + "</div>");
+        var currentIcon = $("<img id='weathericon' src='http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'/>")
         var currentLat = response.coord.lat;
         var currentLon = response.coord.lon;
         
-
         //Displaying weather data to HTML
         var currentCityHeader = $("<div>");
-        currentCityHeader.append($("<h1>" + currentCity + "</h1>"));
-        currentCityHeader.append($("<span>" + today + "</span>"));
+        currentCityHeader.append($("<h1 id='currentCityHeader'>" + currentCity + "</h1>"));
+        currentCityHeader.append($("<span id='date'>" + today + "</span>"));
         currentCityHeader.append(currentIcon);
         $("#citySearchDiv").append(currentTemp);
         $("#citySearchDiv").append(currentHumidity);
@@ -89,7 +85,6 @@ $(document).ready(function() {
       url: "https://api.openweathermap.org/data/2.5/uvi?lat=" +currentLat+ "&lon=" + currentLon + "&appid=" +APIKey,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
       var currentIndex = $("<span class='uv'>" + response.value + "</span>");
       var currentIndexDiv = $("<div id='index'>UV Index: </div>");
       currentIndexDiv.append(currentIndex);
@@ -111,47 +106,38 @@ $(document).ready(function() {
     }).then(function(response) {
       console.log(response)
 
-    var forecastHeader = $("<div id='forecastHeader'><h1> 5 Day Forecast: </h></div>");
-    $("#cityForecastDiv").append(forecastHeader);  
+      //Header for 5 Day forecast
+      var forecastHeader = $("<div id='forecastHeader'><h1> 5 Day Forecast: </h></div>");
+      $("#cityForecastDiv").append(forecastHeader);  
 
-    for (i = 7; i < response.list.length; i+=8) {
-      var forecastIcon = $("<img src='http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png'/>");
-      var forecastTemp = response.list[i].main.temp;
-      var forecastHumidity = response.list[i].main.humidity;
+        //Targeting relevant 5 day forecast information
+        for (i = 7; i < response.list.length; i+=8) {
+        var forecastIcon = $("<img src='http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png'/>");
+        var forecastTemp = response.list[i].main.temp;
+        var forecastHumidity = response.list[i].main.humidity;
 
-      var forecastDateHeader = $("<div id='forecastDateHeader'>");
-      forecastDateHeader.append($("<p class='forecastDate'></p>"));
-      forecastDateHeader.text(moment().add(1, "days").format("dddd D MMM"));
-      forecastDateHeader.append(forecastIcon);
+        //Appending to HTML
+        var forecastDateHeader = $("<div id='forecastDateHeader'>");
+        forecastDateHeader.append($("<p class='forecastDate'></p>"));
+        forecastDateHeader.text(moment().add(i+1, "days").format("dddd D MMM"));
+        forecastDateHeader.append($("<br></br>"));
+        forecastDateHeader.append(forecastIcon);
 
-      var forecast = $("<div id='forecast'</div>");
-      forecast.append(forecastDateHeader);
-      forecast.append($("<p><span class='forecastTemp'> Temp: </span>" + forecastTemp + "ºC</p>"));
-      forecast.append($("<p><span class='forecastHumidity'> Humidity: </span>" + forecastHumidity + "%</p>"));
-      $("#cityForecastDiv").append($(forecast));
-    }
-
-    })
+        var forecast = $("<div id='forecast'</div>");
+        forecast.append(forecastDateHeader);
+        forecast.append($("<p><span class='forecastTemp'> Temp: </span>" + forecastTemp + "ºC</p>"));
+        forecast.append($("<p><span class='forecastHumidity'> Humidity: </span>" + forecastHumidity + "%</p>"));
+        $("#cityForecastDiv").append($(forecast));
+        }
+      })
     });
   })
 }
-
-//Clear search history
-var clearSearch = function () {
-  localStorage.clear();
-  $("#savedCityDiv").empty();
-}
-$("#clearBtn").on("click", clearSearch);
-
- 
-
-
+  //Adding Event Listener for clear search function
+  var clearSearch = function () {
+    localStorage.clear();
+    $("#savedCityDiv").empty();
+  }
+  $("#clearBtn").on("click", clearSearch);
 
 });
-
-
- /* $("#searchFormInput").on("submit", function () {
-    cityInput = $("#searchFormInput").val().trim();
-    console.log(" submit " + cityInput)
-    newCitySearch();
-  });*/
